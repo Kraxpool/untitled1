a0a50d39e5b897a5f6e456d965b50d28c5b7abe6
student <popihacvova@gmail.com>
2022-10-29 13:41:15 +0300

    готово;))

--- Lab_t.py
+++ Lab_t.py
@@ -1,8 +1,3 @@
-s = str(input("Enter string: "))
-for c in s:
-    print(c)
-count = 0;
-for c in s:
-    if c=='a':
-        count = count+1
-        print("count = ", count)
+s = input("Enter string: ")
+n = input('sl->')
+print(f"count = { s.count(n)}")
